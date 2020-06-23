a83ea5b80d7176cb1c75e5d3c64369e057a08d73
Kevin Lin <kevin.lin.p@gmail.com>
2020-06-23 08:28:53 -0400

    parsing by converting to markdown first

--- 1.js
+++ 1.js
@@ -2,10 +2,21 @@ const _ = require('lodash')
 const fs = require('fs')
 const axios = require('axios')
 const cheerio = require('cheerio')
+const TurndownService = require('turndown')
+const turndownService = new TurndownService()
 
-// const letters = _.range('A'.charCodeAt(0), 'W'.charCodeAt(0) + 1).map(i => String.fromCharCode(i));
-// letters.push('XYZ');
-// console.log(letters);
+const one = false
+
+turndownService.addRule('blockquote removal', {
+  filter: 'blockquote',
+  replacement: function(content) {
+    return content
+  },
+})
+
+// const letters = _.range('A'.charCodeAt(0), 'W'.charCodeAt(0) + 1).map(i => String.fromCharCode(i))
+// letters.push('XYZ')
+// console.log(letters)
 
 // axios({
 //   method: 'get',
@@ -20,8 +31,8 @@ const cheerio = require('cheerio')
 let $ = cheerio.load(fs.readFileSync('result/A/index.html'))
 const urls = []
 $('ol li a').each(function() {
-  const url = $(this).attr('href');
-  urls.push(url);
+  const url = $(this).attr('href')
+  urls.push(url)
 })
 
 // urls.forEach(function(url) {
@@ -33,38 +44,49 @@ $('ol li a').each(function() {
 //     .then(function (response) {
 //       response.data.pipe(fs.createWriteStream(`result/A/${url}`))
 //     })
-// });
+// })
 
-urls.forEach(function(url) {
-  console.log(getSong(`result/A/${url}`))
-})
+if (one) {
+  console.log(getSong(`result/A/${urls[0]}`))
+} else {
+  urls.forEach(function(url) {
+    console.log(getSong(`result/A/${url}`))
+  })
+}
 
 function getSong(filename) {
-  $ = cheerio.load(fs.readFileSync(filename))
+  const fileContent = fs.readFileSync(filename, {encoding: 'utf8'})
+  let markdown = turndownService.turndown(fileContent)
 
-  const title = $('h1').text()
+  // markdown = markdown.replaceAll(/$>\w/, '');
+
+  let [title, mainContent] = markdown.split(/=.+/)
+  title = title.match(/\*\*(.+)\*\*/)[1]
 
   let tune = null
+  let tuneReplacer = function (match, p1) {
+    tune = p1
+    return ''
+  }
+  mainContent = mainContent.replace(/\*\*tune:\*\*\s+(.+)/i, tuneReplacer)
+
   let keywords = null
-  const lyrics = []
+  let keywordsReplacer = function (match, p1) {
+    keywords = p1
+    return ''
+  }
+  mainContent = mainContent.replace(/keywords:\s+(.+)/i, keywordsReplacer)
 
-  $('p').each(function() {
-    const text = $(this).text().trim()
-
-    if (text.match(/Tune:/i)) {
-      tune = text
-    } else if (text.match(/keywords:/i)) {
-      keywords = text
-    } else {
-      lyrics.push(text)
-    }
-  })
+  mainContent = mainContent.trim()
+  keywords = keywords.split(',').map(function(v) {return v.trim()})
+  keywords = _.compact(keywords)
+  tune = tune.trim()
 
   return {
     title,
+    mainContent,
     keywords,
     tune,
     filename,
-    lyrics,
   }
 }
