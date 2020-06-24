1d02941e55c3a0684420bea27916a1f3bd1272c3
Kevin Lin <kevin.lin.p@gmail.com>
2020-06-23 20:45:10 -0400

    async bugfix

--- 1-download-letter-indexes.js
+++ 1-download-letter-indexes.js
@@ -4,68 +4,97 @@ const fsPromises = fs.promises
 const axios = require('axios')
 const cheerio = require('cheerio')
 
-function letters() {
-  const letters = _.range('A'.charCodeAt(0), 'W'.charCodeAt(0) + 1).map(i => String.fromCharCode(i))
-  letters.push('XYZ')
+const one = false
+const skipWeb = true
 
-  return letters
+function letters() {
+  if (one) {
+    return ['A']
+  } else {
+    const letters = _.range('A'.charCodeAt(0), 'W'.charCodeAt(0) + 1).map(i => String.fromCharCode(i))
+    letters.push('XYZ')
+
+    return letters
+  }
 }
 
 async function downloadIfNotExist(filename, url) {
-  await fsPromises.access(filename).catch(async function () {
-    await axios({
-      method: 'get',
-      url: url,
-      responseType: 'stream'
-    }).then(function (response) {
-      response.data.pipe(fs.createWriteStream(filename))
-    })
-  })
+  try {
+    await fsPromises.access(filename)
+  } catch {
+    if (!skipWeb) {
+      const response = await axios({
+        method: 'get',
+        url: url,
+        responseType: 'stream'
+      })
+
+      await response.data.pipe(fs.createWriteStream(filename))
+    }
+  }
 
   return filename
 }
 
 async function downloadIndex(letter) {
   const folderPath = `html/${letter}`
+
+  try {
+    await fsPromises.access(folderPath)
+  } catch {
+    await fsPromises.mkdir(folderPath)
+  }
+
   const indexFilename = folderPath + '/index.html';
   const url = `http://www.harrier.net/songbook/${letter}/index.html`
-
-  await fsPromises.access(folderPath).catch(() => {
-    return fsPromises.mkdir(folderPath)
-  })
 
   await downloadIfNotExist(indexFilename, url)
 
   return indexFilename
 }
 
-async function downloadSongs(letter) {
+function parseSongUris(letter) {
   const indexFilename = `html/${letter}/index.html`
   let $ = cheerio.load(fs.readFileSync(indexFilename))
 
   const songUris = []
 
-  $('ol li a').each(function() {
-    const uri = $(this).attr('href')
+  $('ol li a').each((i, e) => {
+    const uri = $(e).attr('href')
     songUris.push(uri)
   })
 
-  const songFilenames = []
+  return songUris
+}
 
-  await Promise.all(songUris.map(async (songUri) => {
+async function downloadSongs(letter) {
+  const songFilenames = []
+  const songUris = parseSongUris(letter)
+
+  const promises = songUris.map(async (songUri) => {
     const songFilename = `html/${letter}/${songUri}`
     const url = `http://www.harrier.net/songbook/${letter}/${songUri}`
 
     songFilenames.push(songFilename)
     await downloadIfNotExist(songFilename, url)
-  }));
+  });
+  await Promise.all(promises)
 
   return songFilenames
 }
 
-letters().forEach(async function (letter) {
-  await downloadIndex(letter)
-  const songFilenames = await downloadSongs(letter)
+async function run() {
+  let allSongFilenames = []
 
-  console.log(songFilenames)
-})
+  const promises = letters().map(async (letter) => {
+    await downloadIndex(letter)
+    const songFilenames = await downloadSongs(letter)
+
+    allSongFilenames += songFilenames
+  })
+  await Promise.all(promises)
+
+  console.log(allSongFilenames)
+}
+
+run()
